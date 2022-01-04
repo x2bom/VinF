b0f2ae621508ebbae80006e4edd41289eeb1613a
x2bom <84973043+x2bom@users.noreply.github.com>
2022-01-04 13:25:04 +0700

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,20 @@
+return [[local a,b = pcall(function()
+      repeat wait(0.25) until game:IsLoaded() and game.Players.LocalPlayer
+      if game.PlaceId == 3978370137 and not game.ReplicatedStorage:FindFirstChild("Matchinfo") then
+          repeat wait() until game.Players.LocalPlayer:FindFirstChild("Loaded") and game.Players.LocalPlayer.Loaded.Value == true 
+      end
+      getgenv().Key = "{0}"
+      local Exploit = (KRNL_LOADED and "Krnl") or (IsElectron and "Electron") or (is_synapse_function and "Synapse") or "ScriptWare"
+      repeat wait() until game:IsLoaded()
+      loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
+  end)
+  if not a then
+      repeat wait(0.25) until game:IsLoaded() and game.Players.LocalPlayer
+      if game.PlaceId == 3978370137 and not game.ReplicatedStorage:FindFirstChild("Matchinfo") then
+          repeat wait() until game.Players.LocalPlayer:FindFirstChild("Loaded") and game.Players.LocalPlayer.Loaded.Value == true 
+      end
+      getgenv().Key = "{0}"
+      local Exploit = (KRNL_LOADED and "Krnl") or (IsElectron and "Electron") or (is_synapse_function and "Synapse") or "ScriptWare"
+      repeat wait() until game:IsLoaded()
+      loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
+  end]]
