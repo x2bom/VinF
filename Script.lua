901844b93d65a0cf4f0bc1c4832d6ec7acfaeee5
x2bom <84973043+x2bom@users.noreply.github.com>
2022-01-06 15:12:12 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,18 +1,22 @@
 
-local a,b = pcall(function()
-    repeat wait(0.25) until game:IsLoaded() and game.Players.LocalPlayer
+local success,err = pcall(function()
+    repeat wait(0.25) until game:IsLoaded()
+    repeat wait(0.25) until game.Players.LocalPlayer
     if (game.PlaceId == 3978370137 or game.PlaceId == 7465136166) and not game.ReplicatedStorage:FindFirstChild("Matchinfo") then
-        repeat wait(0.25) until game.Players.LocalPlayer:FindFirstChild("Loaded") and game.Players.LocalPlayer.Loaded.Value == true 
+        repeat wait(0.25) until game.Players.LocalPlayer:FindFirstChild("Loaded")
+        repeat wait(0.25) until game.Players.LocalPlayer.Loaded.Value == true
     end
     getgenv().Key = "%s"
     local Exploit = (KRNL_LOADED and "Krnl") or (IsElectron and "Electron") or (is_synapse_function and "Synapse") or "ScriptWare"
     loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
 end)
-if not a then
-    print(b)
-    repeat wait(0.25) until game:IsLoaded() and game.Players.LocalPlayer
+if (not success) then
+    rconsoleprint("report this to x2bom : "..err)
+    repeat wait(0.25) until game:IsLoaded()
+    repeat wait(0.25) until game.Players.LocalPlayer
     if (game.PlaceId == 3978370137 or game.PlaceId == 7465136166) and not game.ReplicatedStorage:FindFirstChild("Matchinfo") then
-        repeat wait(0.25) until game.Players.LocalPlayer:FindFirstChild("Loaded") and game.Players.LocalPlayer.Loaded.Value == true 
+        repeat wait(0.25) until game.Players.LocalPlayer:FindFirstChild("Loaded")
+        repeat wait(0.25) until game.Players.LocalPlayer.Loaded.Value == true
     end
     getgenv().Key = "%s"
     local Exploit = (KRNL_LOADED and "Krnl") or (IsElectron and "Electron") or (is_synapse_function and "Synapse") or "ScriptWare"
