e268093463aebcace93cfc7c53eb3f4ef0df42ef
x2bom <84973043+x2bom@users.noreply.github.com>
2022-01-09 18:00:19 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -22,8 +22,28 @@ if (not success) then
     local Exploit = KRNL_LOADED and "Krnl" or "Synapse"
     loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
 end
-wait(120)
+local maps = {
+    ["Sky Islands"] = CFrame.new(-478.53298950195, 49.910675048828, 235.70126342773),
+    ["Tropical Island"] = CFrame.new(-405.94006347656, 82.630447387695, -259.62860107422),
+    ["Mysterious Dungeon"] = CFrame.new(-1522.7856445313, 187.21398925781, -77.721313476563)
+}
+wait(15)
 if not getgenv().ScriptReady then
+    if game.ReplicatedStorage:FindFirstChild("Matchinfo") then
+        if getgenv().namemap == nil and game.PlaceId == 3978370137 and game.ReplicatedStorage:FindFirstChild("Matchinfo") then
+            repeat wait()
+                if game.PlaceId == 3978370137 and game.ReplicatedStorage:FindFirstChild("Matchinfo") then
+                    for a,b in next, workspace.Islands:GetChildren() do
+                        if b:FindFirstChild("SpawnPoint") then
+                            getgenv().namemap = b.Name
+                        end
+                    end
+                end
+            until getgenv().namemap ~= nil
+        end
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = maps[namemap]
+    end
+    wait(105)
     local Exploit = KRNL_LOADED and "Krnl" or "Synapse"
     loadstring(game:HttpGet("https://xn--vn-9ja2dud.com/"..Exploit.."/getscript"))()
 end
