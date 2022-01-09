68160c45001ab2e793dfa2b32ba03c9a711bea32
x2bom <84973043+x2bom@users.noreply.github.com>
2022-01-09 09:43:20 +0700

    Update FruitCheck.lua

--- FruitCheck.lua
+++ FruitCheck.lua
@@ -1,12 +1,15 @@
 local thumbnails = {
-    Tori = 'https://cdn.discordapp.com/attachments/922041161915465749/927790820181045248/238.png',
-    Pika = "https://cdn.discordapp.com/attachments/922041161915465749/927790903387639868/256.png",
-    Magu = "https://cdn.discordapp.com/attachments/922041161915465749/927790997499416616/350.png",
-    Suna = "https://cdn.discordapp.com/attachments/922041161915465749/927791065061290004/194.png",
-    Mera = 'https://cdn.discordapp.com/attachments/922041161915465749/927791150780280892/220.png',
-    Zushi = "https://cdn.discordapp.com/attachments/922041161915465749/927790479972663406/256.png",
-    Gura = "https://cdn.discordapp.com/attachments/922041161915465749/927790516484055040/214.png",
-    Hie = "https://cdn.discordapp.com/attachments/922041161915465749/927790422821072926/256.png",
-    Goro = "https://cdn.discordapp.com/attachments/922041161915465749/927791462500950016/350.png",
+    ["Tori"] = 'https://cdn.discordapp.com/attachments/922041161915465749/927790820181045248/238.png',
+    ["Pika"] = "https://cdn.discordapp.com/attachments/922041161915465749/927790903387639868/256.png",
+    ["Magu"] = "https://cdn.discordapp.com/attachments/922041161915465749/927790997499416616/350.png",
+    ["Suna"] = "https://cdn.discordapp.com/attachments/922041161915465749/927791065061290004/194.png",
+    ["Mera"] = 'https://cdn.discordapp.com/attachments/922041161915465749/927791150780280892/220.png',
+    ["Zushi"] = "https://cdn.discordapp.com/attachments/922041161915465749/927790479972663406/256.png",
+    ["Gura"] = "https://cdn.discordapp.com/attachments/922041161915465749/927790516484055040/214.png",
+    ["Hie"] = "https://cdn.discordapp.com/attachments/922041161915465749/927790422821072926/256.png",
+    ["Goro"] = "https://cdn.discordapp.com/attachments/922041161915465749/927791462500950016/350.png",
+    ["Kraken Blade (Azure)"] = "https://cdn.discordapp.com/emojis/887840316881784852.webp?size=96&quality=lossless",
+    ["Kraken Armor Set (Azure)"] = "https://cdn.discordapp.com/emojis/891873422198599730.webp?size=96&quality=lossless",
+    ["Tomoe Drums"] = "https://cdn.discordapp.com/emojis/861251608892407828.webp?size=96&quality=lossless"
 }
 return thumbnails
