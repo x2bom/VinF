d2e90408dd6b9b62ef1dfd942cf40ed442b792e7
x2bom <84973043+x2bom@users.noreply.github.com>
2022-08-23 21:26:34 +0700

    Update FruitCheck.lua

--- FruitCheck.lua
+++ FruitCheck.lua
@@ -11,6 +11,8 @@ local thumbnails = {
     ["Goro"] = "https://cdn.discordapp.com/attachments/922041161915465749/927791462500950016/350.png",
     ["Kraken Blade (Azure)"] = "https://cdn.discordapp.com/emojis/887840316881784852.webp?size=96&quality=lossless",
     ["Kraken Armor Set (Azure)"] = "https://cdn.discordapp.com/emojis/891873422198599730.webp?size=96&quality=lossless",
-    ["Tomoe Drums"] = "https://cdn.discordapp.com/emojis/861251608892407828.webp?size=96&quality=lossless"
+    ["Tomoe Drums"] = "https://cdn.discordapp.com/emojis/861251608892407828.webp?size=96&quality=lossless",
+    ["Mochi"] = "https://static.wikia.nocookie.net/grand-piece-online/images/a/a2/Mochi_model_test.png/revision/latest?cb=20220531201714",
+    ["Yomi"] = "https://static.wikia.nocookie.net/grand-piece-online/images/2/2e/Yomi2.png/revision/latest?cb=20220528141829"
 }
 return thumbnails
